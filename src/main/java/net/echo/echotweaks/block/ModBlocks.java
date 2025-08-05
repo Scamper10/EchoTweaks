226b8abcdaae7bb90ea14a8e7a2554069baf2a29
Scamper10 <jayhenryc@gmail.com>
2025-08-05 15:07:12 +0100

    Clean up ModBlocks

--- src/main/java/net/echo/echotweaks/block/ModBlocks.java
+++ src/main/java/net/echo/echotweaks/block/ModBlocks.java
@@ -1,5 +1,6 @@
 package net.echo.echotweaks.block;
 
+import java.util.Optional;
 import java.util.function.Function;
 
 import net.echo.echotweaks.EchoTweaks;
@@ -11,6 +12,7 @@ import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.item.Items;
 import net.minecraft.registry.Registries;
@@ -20,46 +22,58 @@ import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
 
 public class ModBlocks {
-	public static void init() {
-		FuelRegistryEvents.BUILD.register((builder, context) -> {
-			builder.add(ModBlocks.CHARCOAL_BLOCK.asItem(), 16_000);
-		});
 
-		ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS)
-			.register((itemGroup) -> itemGroup.addAfter(Items.COAL_BLOCK, CHARCOAL_BLOCK.asItem()));
+	public static class ItemSettingsHelper {
+		public static final Optional<Item.Settings>
+			  DEFAULT_ITEM = Optional.of(new Item.Settings())
+			, NO_ITEM = Optional.empty();
+	} 
 
-		FlammableBlockRegistry.getDefaultInstance().add(CHARCOAL_BLOCK, 5, 5);
-	}
-	
-	private static Block register(String name, AbstractBlock.Settings blockSettings, Item.Settings itemSettings, Boolean shouldRegisterItem, Function<AbstractBlock.Settings, Block> blockFactory) {
-		RegistryKey<Block> blockKey = keyOf(name);
-		Block block = blockFactory.apply(blockSettings.registryKey(blockKey));
-
-		if(shouldRegisterItem) {
-			RegistryKey<Item> itemKey = ModItems.keyOf(name);
-			BlockItem blockItem = new BlockItem(block, itemSettings.registryKey(itemKey));
-			Registry.register(Registries.ITEM, itemKey, blockItem);
-		}
-
-		return Registry.register(Registries.BLOCK, blockKey, block);
-	}
-	private static Block register(String name, AbstractBlock.Settings settings) {
-		return register(
-			name,
-			settings,
-			new Item.Settings(),
-			true,
-			Block::new
-		);
-	}
-	public static RegistryKey<Block> keyOf(String name) {
-		return RegistryKey.of(RegistryKeys.BLOCK, Identifier.of(EchoTweaks.MOD_ID, name));
-	}
-
-	public static final Block CHARCOAL_BLOCK = register(
+	public static final Block CHARCOAL_BLOCK = registerBasic(
 		"charcoal_block",
 		AbstractBlock.Settings.create()
 			.requiresTool()
 			.strength(5.0F, 6.0F)
 	);
+
+
+
+	public static void init() {
+		// Charcoal Block as fuel
+		FuelRegistryEvents.BUILD.register((builder, context) -> {
+			builder.add(ModBlocks.CHARCOAL_BLOCK.asItem(), 16_000);
+		});
+		
+		// Charcoal Block flammable
+		FlammableBlockRegistry.getDefaultInstance().add(CHARCOAL_BLOCK, 5, 5);
+
+		registerInventoryEntries();
+	}
+	
+	private static Block registerBasic(String name, AbstractBlock.Settings blockSettings) {
+		return register(name, blockSettings, ItemSettingsHelper.DEFAULT_ITEM, Block::new);
+	}
+	private static Block register(String name, AbstractBlock.Settings blockSettings, Optional<Item.Settings> itemSettings, Function<AbstractBlock.Settings, Block> blockFactory) {
+		RegistryKey<Block> blockKey = keyOf(name);
+		Block block = blockFactory.apply(blockSettings.registryKey(blockKey));
+
+		if(itemSettings.isPresent()) {
+			RegistryKey<Item> itemKey = ModItems.keyOf(name);
+			BlockItem blockItem = new BlockItem(block, itemSettings.get().registryKey(itemKey));
+			Registry.register(Registries.ITEM, itemKey, blockItem);
+		}
+
+		return Registry.register(Registries.BLOCK, blockKey, block);
+	}
+
+	private static void registerInventoryEntries() {
+		registerInventoryEntry(CHARCOAL_BLOCK, ItemGroups.BUILDING_BLOCKS, Items.COAL_BLOCK);
+	}
+	private static void registerInventoryEntry(Block modBlock, RegistryKey<ItemGroup> group, Item addAfterItem) {
+		ItemGroupEvents.modifyEntriesEvent(group)
+			.register((itemGroup) -> itemGroup.addAfter(addAfterItem, modBlock.asItem()));
+	}
+	public static RegistryKey<Block> keyOf(String name) {
+		return RegistryKey.of(RegistryKeys.BLOCK, Identifier.of(EchoTweaks.MOD_ID, name));
+	}
 }
