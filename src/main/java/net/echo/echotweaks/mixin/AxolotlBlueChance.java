abc96378f9e76e1d56eeb94dfc0f0e38c7ed9357
Scamper10 <jayhenryc@gmail.com>
2025-12-26 10:46:43 +0000

    Remove Blue Axolotl debug chat messages

--- src/main/java/net/echo/echotweaks/mixin/AxolotlBlueChance.java
+++ src/main/java/net/echo/echotweaks/mixin/AxolotlBlueChance.java
@@ -14,7 +14,6 @@ import net.minecraft.entity.passive.AxolotlEntity;
 import net.minecraft.entity.passive.PassiveEntity;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
-import net.minecraft.text.Text;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.world.World;
 
@@ -42,19 +41,15 @@ public abstract class AxolotlBlueChance extends AnimalEntity {
 	private boolean shouldBeDifferent(Random random, ServerWorld world, PassiveEntity other) {
 		final int randomRange = getInverseChance();
 		final int randomChosen = random.nextInt(randomRange);
-		output("Axolotl rolled: " + randomChosen + "/" + randomRange + "  |  Blue: " + (randomChosen==0));
 		return randomChosen == 0;
 	}
 
 	@Unique private int getInverseChance() {
 		Optional<ServerPlayerEntity> loveCause = Optional.ofNullable(this.getLovingPlayer());
-		if(loveCause.isEmpty()) {
-			output("No love cause");
+		if(loveCause.isEmpty()) 
 			return AxolotlEntity.BLUE_BABY_CHANCE;
-		}
 
 		final int baseFromStat = loveCause.get().getStatHandler().getStat(ModStats.BRED, EntityType.AXOLOTL);
-		output("Base from loveCause bred stat: " + baseFromStat);
 		return toInverseChance(baseFromStat);
 	}
 
@@ -63,9 +58,5 @@ public abstract class AxolotlBlueChance extends AnimalEntity {
 
 		// return AxolotlEntity.BLUE_BABY_CHANCE - clamped;
 		return (int) (Math.pow(clamped-BREEDS_FOR_BEST_CHANCE, 2) * MATCH_VANILLA_MULTIPLIER + BEST_INVERSE_CHANCE);
-	}
-
-	@Unique private void output(String text) {
-		this.getEntityWorld().getPlayers().forEach(player -> player.sendMessage(Text.of(text), false));
 	}
 }
