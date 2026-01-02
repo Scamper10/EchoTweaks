e5f5fd74e8a4eac24d4c39b975e88b6303528226
Scamper10 <jayhenryc@gmail.com>
2026-01-02 10:59:17 +0000

    Basic item functionality

---
+++ src/main/java/net/echo/echotweaks/command/NameCommand.java
@@ -0,0 +1,34 @@
+package net.echo.echotweaks.command;
+
+import static com.mojang.brigadier.Command.SINGLE_SUCCESS;
+
+import net.minecraft.command.argument.TextArgumentType;
+import net.minecraft.command.permission.Permission;
+import net.minecraft.command.permission.PermissionLevel;
+import net.minecraft.component.DataComponentTypes;
+import net.minecraft.item.ItemStack;
+import net.minecraft.server.command.CommandManager;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.text.Text;
+
+public class NameCommand {
+	private static final String NAME_TEXT_ARG = "name";
+
+	public static void register() {
+		ModCommands.register("name", (argBuilder, registryAccess) -> {
+			return argBuilder
+				.requires(source -> source.getPermissions().hasPermission(new Permission.Level(PermissionLevel.GAMEMASTERS)))
+				.then(CommandManager.literal("item").then(CommandManager.argument(NAME_TEXT_ARG, TextArgumentType.text(registryAccess))
+					.executes(context -> {
+						// ... maybe check with itemstack.isdamageable
+						ServerCommandSource source = context.getSource();
+						ItemStack heldStack = source.getEntity().getWeaponStack();
+						heldStack.set(DataComponentTypes.ITEM_NAME, Text.of(TextArgumentType.parseTextArgument(context, NAME_TEXT_ARG)));
+						source.sendFeedback(() -> Text.translatable("commands.echotweaks.name.success"), false);
+						return SINGLE_SUCCESS;
+					})
+				));
+				//TODO .then(CommandManager.literal("entity"));
+		});
+	}
+}
