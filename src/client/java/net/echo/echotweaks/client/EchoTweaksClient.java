5f5284644e1df50c62106903bd64ea94dd0066b7
Scamper10 <jayhenryc@gmail.com>
2026-05-15 17:31:33 +0100

    Split main/client environments

---
+++ src/client/java/net/echo/echotweaks/client/EchoTweaksClient.java
@@ -0,0 +1,10 @@
+package net.echo.echotweaks.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EchoTweaksClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
